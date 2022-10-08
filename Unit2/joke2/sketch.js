ce5d6906c1c3e7389cce331c039ce73f433bc90c
kaolald <kaolald@ilstu.edu>
2022-10-07 21:16:38 -0500

    finished joke, touches

--- Unit2/joke2/sketch.js
+++ Unit2/joke2/sketch.js
@@ -15,13 +15,10 @@ function draw() {
   switch(state) {
     case 0: 
     background('grey');
-    text('they call me the PLACEHOLDER TEXT', width/2, height/2);
+    text('what do you get when you cross a rock star and an EDM artist?', width/2, height/2, width/2, height/2);
       break ;
     case 1:
-      text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci. Diam vel quam elementum pulvinar etiam non quam lacus. Ultrices sagittis orci a scelerisque. Nec feugiat nisl pretium fusce id velit. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Eget aliquet nibh praesent tristique magna sit. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis. Egestas maecenas pharetra convallis posuere morbi. Vitae tortor condimentum lacinia quis vel eros donec ac. Id diam maecenas ultricies mi eget mauris pharetra et.', width/2, height/2, 250, 250)
-      break ;
-    case 2:
-
+      text('B4cks1ash/', width/2, height/2, 250, 250);
       break ;
   }
 
--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,7 +1,13 @@
+let img1, img2, img3, img4 ;
+
 let numberOfTouches ;
 
 function setup() {
   createCanvas(400, 400);
+  img1 = loadImage("assets/img1.jpg");
+  img2 = loadImage("assets/img2.jpg");
+  img3 = loadImage("assets/img3.jpg");
+  img4 = loadImage("assets/img4.jpg");
 }
 
 function draw() {
@@ -12,20 +18,24 @@ function draw() {
   switch(numberOfTouches) {
     case 0: 
     text("where is everyone?", 5, 22) ; 
+    image(img1, 0, 50, 400, 400);
       break ;
       
       case 1: 
        text("oh... hi", 5, 22) ; 
+       image(img2, 0, 50, 400, 400);
       // put a picture here
       break ;
       
       case 2:
-      text("oh ok theres two now", 5, 22) ; 
+      text("oh ok theres more now", 5, 22) ; 
+      image(img3, 0, 50, 400, 400);
             // put a picture here
       break ;
       
       case 3:
      text("WHERE DID YOU ALL COME FROM", 5, 22) ; 
+     image(img4, 0, 50, 400, 400);
             // put a picture here
       break ;
     
